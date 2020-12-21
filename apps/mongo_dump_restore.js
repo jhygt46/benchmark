efc981daf0c758549673ac2ca0568fa87cb1395f
jhygt46 <infolifeoperacional@gmail.com>
2020-12-20 23:54:01 -0300

    aa

--- apps/mongo_dump_restore.js
+++ apps/mongo_dump_restore.js
@@ -1,11 +1,26 @@
 var app = require('http').createServer(serverfunc);
 app.listen(80, () => { console.log("RUNNING 81") });
 
+var spawn = require('child_process').spawn;
 const mongodb = require('mongodb');
 
 var db;
 const connectionString = 'mongodb://myTester:buenanelson@34.121.247.48:27017/test';
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, client) { db = client.db() });
+
+setTimeout(()=>{
+    var args = ['--host', '34.121.247.48:27017', '--username', 'myTester', '--password', 'buenanelson', '--port', '27017', '--db', 'test', '--collection', 'hola', '--out', '/var/node/mongodump-2011-10-24'];
+    var mongodump = spawn('/usr/local/bin/mongodump', args);
+    mongodump.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+    });
+    mongodump.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+    mongodump.on('exit', function (code) {
+      console.log('mongodump exited with code ' + code);
+    });
+}, 2000)
 
 
 async function serverfunc(req, res){
